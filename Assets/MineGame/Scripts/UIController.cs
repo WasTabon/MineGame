3727bf4fc0dad189aafdf36b05fb51b8d96c4423
WasTabon <titanium.jen@gmail.com>
2025-11-06 14:33:36 +0200

    asset

---
+++ Assets/MineGame/Scripts/UIController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController Instance;
+     
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    
+}
