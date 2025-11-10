85d58dd4e1e0da021e73b6eab78a139735716eb4
WasTabon <titanium.jen@gmail.com>
2025-11-10 17:01:20 +0200

    fix

--- Assets/MineGame/Scripts/RobotFollower.cs
+++ Assets/MineGame/Scripts/RobotFollower.cs
@@ -41,7 +41,6 @@ public class RobotFollower : MonoBehaviour
         {
             _rb.interpolation = RigidbodyInterpolation.Interpolate;
             _rb.drag = drag;
-            _rb.constraints = RigidbodyConstraints.FreezeRotation;
         }
         
         if (player == null)
